5c089a09db384d583eeb233585844d6bbc178d94
Matheus Doreto <matheus.alencar.st@prometeon.com>
2022-07-26 12:36:30 -0300

    .

--- backend/main.py
+++ backend/main.py
@@ -4,9 +4,11 @@ from pydantic import BaseSettings
 from functools import lru_cache
 from google.oauth2 import id_token
 from google.auth.transport import requests
+from fastapi.security import HTTPBasicCredentials, HTTPBearer
 import requests as req
 
 app = FastAPI()
+security = HTTPBearer()
 
 class Settings(BaseSettings):
     client_secret:str
@@ -30,12 +32,11 @@ async def root():
     return {"msg": "Hello World"}
 
 @app.post("/user/google")
-def auth_google(code: str = Form(), client_id: str = Form()):
+async def auth_google(code: str = Form(), client_id: str = Form()):
     settings = get_settings()
     url='https://accounts.google.com/o/oauth2/token'
     params = {"grant_type":"authorization_code","code":code,"client_id":client_id,"client_secret":settings.client_secret, "redirect_uri":settings.ui_url +"login"}
-    r =  req.post(url, params=params).json()
-    print(r)
+    r = await req.post(url, params=params).json()
     try:
         idinfo = id_token.verify_oauth2_token(r['id_token'], requests.Request(), client_id)
         userid = idinfo['sub']
@@ -44,5 +45,6 @@ def auth_google(code: str = Form(), client_id: str = Form()):
         return None
     
 @app.get("/auth/user")
-async def get_user():
-    return None
+async def get_user(credentials: HTTPBasicCredentials = Depends(security)):
+    token = credentials.credentials
+    return token
