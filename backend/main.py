3fc454198e18af5e3550873ae415a21bee330eba
Matheus Doreto <matheus.alencar.st@prometeon.com>
2022-07-26 12:25:05 -0300

    .

--- backend/main.py
+++ backend/main.py
@@ -30,11 +30,12 @@ async def root():
     return {"msg": "Hello World"}
 
 @app.post("/user/google")
-async def auth_google(code: str = Form(), client_id: str = Form()):
+def auth_google(code: str = Form(), client_id: str = Form()):
     settings = get_settings()
     url='https://accounts.google.com/o/oauth2/token'
     params = {"grant_type":"authorization_code","code":code,"client_id":client_id,"client_secret":settings.client_secret, "redirect_uri":settings.ui_url +"login"}
-    r = await req.post(url, params=params).json()
+    r =  req.post(url, params=params).json()
+    print(r)
     try:
         idinfo = id_token.verify_oauth2_token(r['id_token'], requests.Request(), client_id)
         userid = idinfo['sub']
