7974e0e9e898f52367ea875701c2d7158dc786fc
Matheus Doreto <matheus.alencar.st@prometeon.com>
2022-07-26 14:25:52 -0300

    .

--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,18 @@
-from fastapi import FastAPI, Form, Depends
+from fastapi import FastAPI, Form
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseSettings
 from functools import lru_cache
 from google.oauth2 import id_token
 from google.auth.transport import requests
-from fastapi.security import HTTPBasicCredentials, HTTPBearer
 import requests as req
 
 app = FastAPI()
-security = HTTPBearer()
-
 class Settings(BaseSettings):
     client_secret:str
     ui_url:str
+    client_id:str
+    class Config:
+        env_file='.env'
 
 @lru_cache()
 def get_settings():
@@ -32,19 +32,18 @@ async def root():
     return {"msg": "Hello World"}
 
 @app.post("/user/google")
-async def auth_google(code: str = Form(), client_id: str = Form()):
+async def auth_google(code: str = Form()):
     settings = get_settings()
     url='https://accounts.google.com/o/oauth2/token'
-    params = {"grant_type":"authorization_code","code":code,"client_id":client_id,"client_secret":settings.client_secret, "redirect_uri":settings.ui_url +"login"}
+    params = {"grant_type":"authorization_code","code":code,"client_id":settings.client_id,"client_secret":settings.client_secret, "redirect_uri":settings.ui_url +"login"}
     r = await req.post(url, params=params).json()
     try:
-        idinfo = id_token.verify_oauth2_token(r['id_token'], requests.Request(), client_id)
+        idinfo = id_token.verify_oauth2_token(r['id_token'], requests.Request(), settings.client_id)
         userid = idinfo['sub']
         return userid
     except ValueError:
         return None
     
 @app.get("/auth/user")
-async def get_user(credentials: HTTPBasicCredentials = Depends(security)):
-    token = credentials.credentials
-    return token
+async def get_user():
+    return None
