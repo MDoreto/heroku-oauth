da460b3743cc7a9b426031e41a1c2dedd1f8408f
Matheus Doreto <matheus.alencar.st@prometeon.com>
2022-07-26 14:48:51 -0300

    .

--- backend/main.py
+++ backend/main.py
@@ -1,24 +1,16 @@
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, Form, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseSettings
 from functools import lru_cache
 from google.oauth2 import id_token
 from google.auth.transport import requests
+from fastapi.security import HTTPBasicCredentials, HTTPBearer
 import requests as req
 
 app = FastAPI()
-class Settings(BaseSettings):
-    client_secret:str
-    ui_url:str
-    client_id:str
-    class Config:
-        env_file='.env'
 
-@lru_cache()
-def get_settings():
-    return Settings()
-
-origins=[get_settings().ui_url]
+security = HTTPBearer()
+origins=["https://prometeon-frontend.herokuapp.com/","https://prometeon-frontend.herokuapp.com","*"]
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -27,23 +19,35 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+class Settings(BaseSettings):
+    client_id:str
+    client_secret:str
+    ui_url:str
+
+@lru_cache()
+def get_settings():
+    return Settings()
+
 @app.get("/")
 async def root():
     return {"msg": "Hello World"}
 
 @app.post("/user/google")
-async def auth_google(code: str = Form()):
+def auth_google(code: str = Form(), client_id: str = Form()):
     settings = get_settings()
     url='https://accounts.google.com/o/oauth2/token'
-    params = {"grant_type":"authorization_code","code":code,"client_id":settings.client_id,"client_secret":settings.client_secret, "redirect_uri":settings.ui_url +"login"}
-    r = await req.post(url, params=params).json()
+    params = {"grant_type":"authorization_code","code":code,"client_id":client_id,"client_secret":settings.client_secret, "redirect_uri":settings.ui_url +"/login"}
+    r = req.post(url, params=params).json()
+    print(r)
     try:
-        idinfo = id_token.verify_oauth2_token(r['id_token'], requests.Request(), settings.client_id)
+        idinfo = id_token.verify_oauth2_token(r['id_token'], requests.Request(), client_id)
         userid = idinfo['sub']
         return userid
     except ValueError:
+        print("Error")
         return None
     
 @app.get("/auth/user")
-async def get_user():
-    return None
+async def get_user(credentials: HTTPBasicCredentials = Depends(security)):
+    token = credentials.credentials
+    return token
