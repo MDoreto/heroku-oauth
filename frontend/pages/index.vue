06774c3e04d06b3098f6086712dc0983e82dd1e8
MDoreto <matheusdoreto.md@gmail.com>
2022-08-01 12:52:13 -0300

    .

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -7,7 +7,7 @@
       </v-card>
       <v-card>
         <v-card-title class="headline">
-          Welcome to the Prometeon Template
+          Welcome to the Prometeon Official template
         </v-card-title>
         <v-card-text>
           <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
